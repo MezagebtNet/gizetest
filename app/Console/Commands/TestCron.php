8feac4e0f9d1aa44011248c5990dbb93ab3ffc68
Girum S <4girum@gmail.com>
2021-09-05 07:27:28 +0300

    cron job test

---
+++ app/Console/Commands/TestCron.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \Log::info("Cron is working fine!");
+        /*
+
+        Write your database logic we bellow:
+
+        User::create(['name'=>'test', 'email' => 'test@test.com']);
+
+         */
+
+        // return 0;
+    }
+}
