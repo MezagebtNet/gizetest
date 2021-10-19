3b67a1719dd1bd50bc762e1cb8ecc30dadd59e7b
Girum S <4girum@gmail.com>
2021-10-19 13:31:48 +0300

    fixed copy link url for youtube videos

--- resources/views/website/play/index.blade.php
+++ resources/views/website/play/index.blade.php
@@ -302,7 +302,7 @@
     <!-- Modal -->
     <div class="modal fade" id="shareModal"  data-keyboard="false" tabindex="-1"
         aria-labelledby="shareModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="shareModalLabel">{{ __('Share') }}</h5>
@@ -388,6 +388,9 @@
                 videojs("player").landscapeFullscreen();
 
             });
+        });
+
+        $(function() {
 
             $('.btn-copy').on('click', function(){
                 var copyText = $("#shareUrl");
